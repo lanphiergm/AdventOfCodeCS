23c7504b498a95f770401fa6c9e24ef9c21f7df0
Galen Lanphier <Galen.Lanphier@jacobs.com>
2020-12-10 09:58:44 -0500

    Added 2020 day 10 and 2015 day 14

---
+++ Problems/Year2015/Day14ReindeerOlympics.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Problems.Year2015
+{
+    class Day14ReindeerOlympics : ProblemBase<int>
+    {
+        public Day14ReindeerOlympics(ILogger logger) : base(logger, "Reindeer Olympics", 2015, 14) { }
+
+        protected override int ExecutePart1()
+        {
+            int maxDist = 0;
+            foreach (Reindeer r in ParseReindeer())
+            {
+                int dist = r.GetDistance(duration);
+                if (dist > maxDist)
+                {
+                    maxDist = dist;
+                }
+            }
+            return maxDist;
+        }
+
+        protected override int ExecutePart2()
+        {
+            var reindeer = ParseReindeer();
+            for (int i = 1; i <= duration; i++)
+            {
+                var distances = new List<(int distance, Reindeer deer)>();
+                foreach(Reindeer r in reindeer)
+                {
+                    distances.Add((r.GetDistance(i), r));
+                }
+                int maxDist = distances.Max(i => i.distance);
+                foreach (var (distance, deer) in distances.Where(i => i.distance == maxDist))
+                {
+                    deer.Score++;
+                }
+            }
+
+            return reindeer.Max(r => r.Score);
+        }
+
+        private static List<Reindeer> ParseReindeer()
+        {
+            var reindeer = new List<Reindeer>();
+            foreach (string capability in capabilities)
+            {
+                var match = capabilityRegex.Match(capability);
+                reindeer.Add(new Reindeer(match.Groups[1].Value, int.Parse(match.Groups[2].Value),
+                    int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value)));
+            }
+            return reindeer;
+        }
+
+        private class Reindeer
+        {
+            private readonly int speed;
+            private readonly int cycleTime;
+            private readonly int flyTime;
+
+            public Reindeer(string name, int speed, int flyTime, int restTime)
+            {
+                Name = name;
+                this.speed = speed;
+                this.flyTime = flyTime;
+                cycleTime = flyTime + restTime;
+            }
+
+            public string Name { get; }
+            public int Score { get; set; }
+
+            public int GetDistance(int seconds)
+            {
+                int wholeCycles = seconds / cycleTime;
+                int remainingSeconds = seconds % cycleTime;
+                return wholeCycles * flyTime * speed + Math.Min(remainingSeconds, flyTime) * speed;
+            }
+        }
+
+        private static readonly Regex capabilityRegex = new Regex(
+            "(.*) can fly (\\d*) km/s for (\\d*) seconds, but then must rest for (\\d*) seconds.");
+
+        #region Data
+
+        private const int duration = 2503;
+
+        private static readonly string[] capabilities =
+        {
+            "Vixen can fly 8 km/s for 8 seconds, but then must rest for 53 seconds.",
+            "Blitzen can fly 13 km/s for 4 seconds, but then must rest for 49 seconds.",
+            "Rudolph can fly 20 km/s for 7 seconds, but then must rest for 132 seconds.",
+            "Cupid can fly 12 km/s for 4 seconds, but then must rest for 43 seconds.",
+            "Donner can fly 9 km/s for 5 seconds, but then must rest for 38 seconds.",
+            "Dasher can fly 10 km/s for 4 seconds, but then must rest for 37 seconds.",
+            "Comet can fly 3 km/s for 37 seconds, but then must rest for 76 seconds.",
+            "Prancer can fly 9 km/s for 12 seconds, but then must rest for 97 seconds.",
+            "Dancer can fly 37 km/s for 1 seconds, but then must rest for 36 seconds."
+        };
+
+        #endregion Data
+    }
+}
---
+++ Problems/Year2020/Day10AdapterArray.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Problems.Year2020
+{
+    class Day10AdapterArray : ProblemBase<long>
+    {
+        public Day10AdapterArray(ILogger logger) : base(logger, "Adapter Array", 2020, 10) { }
+
+        protected override long ExecutePart1()
+        {
+            var sorted = adapters.OrderBy(a => a);
+            int jump1Count = 0;
+            int jump3Count = 1; // the built-in adapter is 3 jolts
+            int currJoltage = 0;
+            foreach(int adapter in sorted)
+            {
+                if (adapter == currJoltage + 1)
+                {
+                    jump1Count++;
+                }
+                else if (adapter == currJoltage + 3)
+                {
+                    jump3Count++;
+                }
+                currJoltage = adapter;
+            }
+            return jump1Count * jump3Count;
+        }
+
+        protected override long ExecutePart2()
+        {
+            var sorted = adapters.OrderBy(a => a).ToList();
+            var possibilities = new Dictionary<int, long>();
+            int builtInJoltage = sorted[^1] + 3;
+            for (int i = sorted.Count - 1; i >= 0; i--)
+            {
+                long possibilityCount = 0;
+                for (int j = i + 1; j <= i + 3; j++)
+                {
+                    if (j == sorted.Count && builtInJoltage - sorted[i] <= 3)
+                    {
+                        possibilityCount++;
+                        break;
+                    }
+                    else if (j == sorted.Count)
+                    {
+                        break;
+                    }
+                    else if (sorted[j] - sorted[i] <= 3)
+                    {
+                        possibilityCount += possibilities[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                possibilities[i] = possibilityCount;
+            }
+
+            long sum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (sorted[i] < 3)
+                {
+                    sum += possibilities[i];
+                }
+            }
+            return sum;
+        }
+
+        #region Data
+
+        private static readonly int[] adapters =
+        {
+            35,
+            111,
+            135,
+            32,
+            150,
+            5,
+            106,
+            154,
+            41,
+            7,
+            27,
+            117,
+            109,
+            63,
+            64,
+            21,
+            138,
+            98,
+            40,
+            71,
+            144,
+            13,
+            66,
+            48,
+            12,
+            55,
+            119,
+            103,
+            54,
+            78,
+            65,
+            112,
+            39,
+            128,
+            53,
+            140,
+            77,
+            34,
+            28,
+            81,
+            151,
+            125,
+            85,
+            124,
+            2,
+            99,
+            131,
+            59,
+            60,
+            6,
+            94,
+            33,
+            42,
+            93,
+            14,
+            141,
+            92,
+            38,
+            104,
+            9,
+            29,
+            100,
+            52,
+            19,
+            147,
+            49,
+            74,
+            70,
+            84,
+            113,
+            120,
+            91,
+            97,
+            17,
+            45,
+            139,
+            90,
+            116,
+            149,
+            129,
+            87,
+            69,
+            20,
+            24,
+            148,
+            18,
+            58,
+            123,
+            76,
+            118,
+            130,
+            132,
+            75,
+            110,
+            105,
+            1,
+            8,
+            86
+        };
+
+        #endregion Data
+    }
+}
