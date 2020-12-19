4cd3504fe2130af26553105e71e1e2283d4c67d4
lanphiergm <lanphiergm@gmail.com>
2020-12-19 07:41:05 -0500

    Added 2020 day 18

---
+++ StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Contains extensions of the string class
+    /// </summary>
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Replaces only the first occurance of oldValue with newValue
+        /// </summary>
+        /// <param name="instance">The string to use as input</param>
+        /// <param name="oldValue">The old value to find</param>
+        /// <param name="newValue">The new value to replace with</param>
+        /// <returns>The modified string</returns>
+        public static string ReplaceOnce(this string instance, string oldValue, string newValue)
+        {
+            var regex = new Regex(Regex.Escape(oldValue));
+            return regex.Replace(instance, newValue, 1);
+        }
+    }
+}
